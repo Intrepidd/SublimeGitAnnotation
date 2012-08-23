d1196f46309a2a862a7c91703c00fbf7d0a9dca5
Intrepidd <adrien.siami@gmail.com>
2012-08-23 21:56:21 +0200

    First experimentation, adds a colored dot when the file changes in the position of the carret

--- SublimeGitAnnotation.py
+++ SublimeGitAnnotation.py
@@ -2,4 +2,5 @@ import sublime, sublime_plugin
 
 class AnnotationListener(sublime_plugin.EventListener):
   def on_modified(self, view):
-    print view.size()
+    region = view.sel()[0]
+    view.add_regions('edited', [region] + view.get_regions('edited'), 'variable.parameter', 'dot')
